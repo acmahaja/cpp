9e66f3bbb7db960b7367ab597746e2538f11d9cb
Anjaney <acmahaja@ncsu.edu>
2019-12-22 14:20:52 -0500

    created function prototype file

---
+++ function_prototype/function_prototype.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int foo(int x);
+
+int main()
+{
+    foo(2);
+}
+
+int foo(int)
+{
+        
+}
+
--- functions/functions.cpp
+++ functions/functions.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
+int add3(int i, int j=3);
+int add2(int i);
 
-int add2(int i)   // Declaration of add2
-{
-    int j = i + 2;
-    return j;
-}
 //  Note: add2 is still missing a DEFINITOIN
 //  Even though it doesn't appear directly in code,
 //  add2 definition may be LINKED in from another object file.
@@ -12,5 +9,18 @@ int add2(int i)   // Declaration of add2
 int main()
 {
     std::cout << add2(2) << "\n";    // add2(2) will be evaluated at this point
+    std::cout << add3(2,2) << "\n";
     return 0;
+}
+
+int add2(int i, int j)
+{
+    int k = i + j + 2;
+    return k;
+}
+
+int add2(int i)   // Declaration of add2
+{
+    int j = i + 2;
+    return j;
 }
